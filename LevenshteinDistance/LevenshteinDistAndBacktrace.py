24a1e45eaad6c560dd7fa56351d7a973ed0471a3
Aydar <aydar710@gmail.com>
2020-05-27 14:59:50 +0300

    change replacement operation cost

--- LevenshteinDistance/LevenshteinDistAndBacktrace.py
+++ LevenshteinDistance/LevenshteinDistAndBacktrace.py
@@ -19,7 +19,7 @@ def get_cost_matrix(s1, s2):
         for j, c2 in enumerate(s2):
             insertions = previous_row[j + 1] + 1
             deletions = current_row[j] + 1
-            substitutions = previous_row[j] + (c1 != c2)
+            substitutions = previous_row[j] + (c1 != c2) * 2
             current_row.append(min(insertions, deletions, substitutions))
         previous_row = current_row
 
